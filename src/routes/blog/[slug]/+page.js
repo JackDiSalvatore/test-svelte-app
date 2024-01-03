9a0336cb910c9c2ac490d4d44b43dd59cca1caea
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-03 13:52:44 -0600

    converting load function to async to mimick what an acutal API call would do

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,13 +1,19 @@
 import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
-export function load({ params }) {
+export async function load({ params }) {
 
 	try {
 		return {
 			title: `Title for "${params.slug}" goes here`,
-			content: `Content for "${params.slug}" goes here`,
-			parentData: `Parent Blog data goes here`
+			content: `Blog post conntent for "${params.slug}" goes here.  This is my blog post.  I hope you enjoy reading it!`,
+			parentData: `Parent Blog data goes here`,
+			comments: [
+				{
+					'author': 'Jack',
+					'content': 'Lorem ipsum dolar ...'
+				}
+			]
 		};
 	} catch (e) {
 		error(404, 'Not found ' + e);
