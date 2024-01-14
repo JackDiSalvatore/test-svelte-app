25efa21915b0c977ca443a8df7d7511a0e65ab96
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-01-14 14:54:10 -0600

    simple pokemon api call
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ src/routes/pokemon/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<h1>{data.name}</h1>
+
+<img src={data.sprites.front_default} alt="front-default"/>
+
+<h3>Abilities</h3>
+<ul>
+	{#each data.abilities as ability}
+		<li>{ability.ability.name}</li>
+	{/each}
+</ul>
+
+<h3>Moves</h3>
+<ul>
+	{#each data.moves as move}
+		<li>{move.move.name}</li>
+	{/each}
+</ul>
