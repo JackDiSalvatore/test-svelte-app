b20f74aec82cc7d3c02d27b946a9e485f7809516
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-02-19 09:10:33 -0600

    creating my own card component
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,23 @@
+<section>
+  <slot name="title" />
+  <slot name="description" />
+
+  <footer>
+    <slot />
+  </footer>
+</section>
+
+<style>
+  section {
+    width: var(--card-width);
+    margin: var(--card-margin);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid grey;
+    border-radius: 0.375rem;
+    background-color: var(--card-background-color);
+    color: var(--card-color);
+  }
+</style>
--- src/routes/pokemon/+page.svelte
+++ src/routes/pokemon/+page.svelte
@@ -1,21 +1,54 @@
 <script lang="ts">
-	export let data;
+  import Card from "$lib/components/Card.svelte";
+  export let data;
 </script>
 
-<h1>{data.name}</h1>
+<section>
+  <Card
+    --card-width="50%"
+    --card-margin="0.5rem"
+    --card-background-color="grey"
+    --card-color="aliceblue"
+  >
+    <h1 slot="title">
+      {data.name}
+      <img src={data.sprites.front_default} alt="pokemon" />
+    </h1>
+    <p slot="description">Description...</p>
+    <p>card footer</p>
+  </Card>
 
-<img src={data.sprites.front_default} alt="front-default"/>
+  <Card --card-width="50%" --card-margin="0.5rem">
+    <h1 slot="title">Abilities</h1>
+    <ul slot="description">
+      {#each data.abilities as ability}
+        <li>{ability.ability.name}</li>
+      {/each}
+    </ul>
+  </Card>
 
-<h3>Abilities</h3>
-<ul>
-	{#each data.abilities as ability}
-		<li>{ability.ability.name}</li>
-	{/each}
-</ul>
+  <Card --card-width="50%" --card-margin="0.5rem">
+    <h1 slot="title">Moves</h1>
+    <ul slot="description">
+      {#each data.moves as move}
+        <li>{move.move.name}</li>
+      {/each}
+    </ul>
+  </Card>
+</section>
 
-<h3>Moves</h3>
-<ul>
-	{#each data.moves as move}
-		<li>{move.move.name}</li>
-	{/each}
-</ul>
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem;
+  }
+  h1 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
